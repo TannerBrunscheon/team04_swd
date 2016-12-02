58c57848c23a7f70dc69016add9da8e40c31a7d1
Tanner <tanner-brunscheon@uiowa.edu>
2016-12-02 00:43:41 -0600

    Wow authorization works
    (cherry picked from commit 2f450ae)

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,5 @@
 
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -8,15 +9,21 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.DataStoreFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.fusiontables.Fusiontables;
 import com.google.api.services.fusiontables.FusiontablesScopes;
+import com.google.api.services.fusiontables.model.Column;
+import com.google.api.services.fusiontables.model.Table;
+
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.UUID;
 
 
-
-    /**
+/**
      * Created by Tanner on 12/1/2016.
      */
     public class Server {
@@ -24,7 +31,7 @@ import java.util.Collections;
 
         private static final String TABLE_ID = "1jigM5G7EHZlGhAWETMo76ZU2V54oWEniS0T28RoM&";
 
-        private static FileDataStoreFactory dataStoreFactory;
+        private static DataStoreFactory dataStoreFactory;
 
         private static final java.io.File CREDSTORE =
                 new java.io.File("team04_swd/Credentials/");
@@ -35,16 +42,14 @@ import java.util.Collections;
 
         private static Fusiontables fusiontables;
 
-
         private static Credential access()throws Exception{
-            GoogleClientSecrets secrets = GoogleClientSecrets.load(
-                    JSON_FACTORY, new InputStreamReader(
-                            Server.class.getResourceAsStream("/client_id.json")));
+            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
+                    JSON_FACTORY,new InputStreamReader(Server.class.getResourceAsStream("/client_id.json")));
 
             GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                    httpTransport, JSON_FACTORY, secrets,
-                    Collections.singleton(FusiontablesScopes.FUSIONTABLES)).setDataStoreFactory(
-                    dataStoreFactory).build();
+                    httpTransport, JSON_FACTORY, clientSecrets,Collections.singleton(FusiontablesScopes.FUSIONTABLES)).setDataStoreFactory(dataStoreFactory).build();
+
+
 
             return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
         }
@@ -55,12 +60,22 @@ import java.util.Collections;
                 httpTransport = GoogleNetHttpTransport.newTrustedTransport();
                 dataStoreFactory = new FileDataStoreFactory(CREDSTORE);
                 Credential credential = access();
+
                 fusiontables = new Fusiontables.Builder(httpTransport,JSON_FACTORY,credential).setApplicationName(APPLICATION_NAME).build();
-                Fusiontables.Query.Sql sql = fusiontables.query().sql("SELECT Text,Number,Location,Date FROM " + TABLE_ID);
+                Table table = new Table();
+                table.setName(UUID.randomUUID().toString());
+                table.setIsExportable(false);
+                table.setDescription("Sample Table");
 
+                // Set columns for new table
+                table.setColumns(Arrays.asList(new Column().setName("Text").setType("STRING"),
+                        new Column().setName("Number").setType("NUMBER"),
+                        new Column().setName("Location").setType("LOCATION"),
+                        new Column().setName("Date").setType("DATETIME")));
 
-
-
+                // Adds a new column to the table.
+                Fusiontables.Table.Insert t = fusiontables.table().insert(table);
+                Table r = t.execute();
             }
             catch (Exception e){
 
