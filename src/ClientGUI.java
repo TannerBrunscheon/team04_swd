01a662843587d9ff04494f523c76e79dce87015b
Tom Miksch <thomas-miksch@uiowa.edu>
2016-12-03 10:19:06 -0600

    Client stuff

---
+++ src/ClientGUI.java
@@ -0,0 +1,18 @@
+/**
+ * Created by tmiksch on 12/3/16.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
