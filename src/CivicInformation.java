aada1a354fd2186633fed9820314e5b4c12e978e
Tanner <tanner-brunscheon@uiowa.edu>
2016-12-03 15:19:25 -0600

    Civics Added

--- src/CivicInformation.java
+++ src/CivicInformation.java
@@ -4,27 +4,51 @@ import com.google.api.services.civicinfo.CivicInfoRequestInitializer;
 import com.google.api.services.civicinfo.model.GeographicDivision;
 import com.google.api.services.civicinfo.model.RepresentativeInfoResponse;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Tanner on 12/3/2016.
  */
 public class CivicInformation {
-    //public static String  getRepInfo(String address){
-    public static void main(String[] args) {
+    public static String[] getCounty(String address) {
+        String[] strings = new String[2];
         try {
-            CivicInfo civicInfo = new CivicInfo.Builder(GoogleNetHttpTransport.newTrustedTransport(),toFusionTableTest.JSON_FACTORY,null)
-                .setApplicationName("ServerForSWD").setGoogleClientRequestInitializer(new CivicInfoRequestInitializer("AIzaSyA-ZVQaE0mN6lEsE5Z1hgppGa0J6oSoDA8")).build();
+            CivicInfo civicInfo = new CivicInfo.Builder(GoogleNetHttpTransport.newTrustedTransport(), toFusionTableTest.JSON_FACTORY, null)
+                    .setApplicationName("ServerForSWD").setGoogleClientRequestInitializer(new CivicInfoRequestInitializer("AIzaSyA-ZVQaE0mN6lEsE5Z1hgppGa0J6oSoDA8")).build();
+            RepresentativeInfoResponse response = civicInfo.representatives().representativeInfoByAddress().setAddress(address).execute();
+            Map<String, GeographicDivision> map = response.getDivisions();
+            Object[] key = map.keySet().toArray();
 
-        String address ="607 Park View Drive Readlyn Iowa 50668";
+            for (int i = 0; i < key.length; i++) {
+                String string = key[i].toString();
+                if (string.contains("cd:")) {
+                    String[] strtemp = string.split("/");
+                    for (int j = 0; j < strtemp.length; j++) {
+                        if (strtemp[j].contains("cd:")) ;
+                        {
+                            strings[0] = strtemp[j].substring(3);
+                        }
+                    }
+                } else if (string.contains("county")) {
+                    String[] strtemp = string.split("/");
+                    for (int j = 0; j < strtemp.length; j++) {
+                        if (strtemp[j].contains("county")) ;
+                        {
+                            strings[1] = strtemp[j].substring(7);
+                        }
+                    }
+                }
+            }
+            System.out.println(strings[0]);
+            System.out.println(strings[1]);
 
 
-            RepresentativeInfoResponse response=civicInfo.representatives().representativeInfoByAddress().setAddress(address).execute();
-            Map<String,GeographicDivision> map=response.getDivisions();
-            map.toString();
+        } catch (Exception e) {
+
         }
-        catch (Exception e){
-
-        }
+        return strings;
     }
 }
