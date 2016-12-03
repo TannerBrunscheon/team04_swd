ff2d3e1fb3809d8cfac90ded2d61e03bd6cb7916
Tom Miksch <thomas-miksch@uiowa.edu>
2016-12-03 10:26:01 -0600

    Client stuff

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -3,6 +3,9 @@
  */
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ClientGUI extends Application {
@@ -12,7 +15,12 @@ public class ClientGUI extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws Exception{
+        Parent root =
+                FXMLLoader.load(getClass().getResource("ClientFX.fxml"));
+        Scene scene = new Scene(root); // attach scene graph to scene
+        primaryStage.setTitle("Voter Ballot"); // displayed in window's title bar
+        primaryStage.setScene(scene); // attach scene to stage
+        primaryStage.show(); // display the stage
     }
 }
