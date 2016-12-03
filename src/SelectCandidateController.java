3f4ea5ec6f1e741cd1a69fcdf4cd3fcb170bfbef
Tom Miksch <thomas-miksch@uiowa.edu>
2016-12-03 12:29:11 -0600

    Client stuff

--- src/SelectCandidateController.java
+++ src/SelectCandidateController.java
@@ -1,5 +1,7 @@
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 
 /**
@@ -12,9 +14,23 @@ public class SelectCandidateController {
     private ChoiceBox senateSelect;
     @FXML
     private ChoiceBox houseSelect;
+    @FXML
+    private Button voteButton;
 
     @FXML
     private void initialize() {
         presidentSelect.setItems(FXCollections.observableArrayList("Bob Saget", "Other Guy"));
+        senateSelect.setItems(FXCollections.observableArrayList("I guess", "Other Guy"));
+        houseSelect.setItems(FXCollections.observableArrayList("for sure", "Other Guy"));
+    }
+
+    @FXML
+    private void votePressed(ActionEvent event){
+        String candidates = presidentSelect.getValue() + ", " + senateSelect.getValue() + ", " + houseSelect.getValue();
+
+        if (presidentSelect.getValue() == null || senateSelect.getValue() == null || houseSelect.getValue() == null){
+
+        }
+        System.out.println(candidates);
     }
 }
