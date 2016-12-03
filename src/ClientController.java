93b44fa45262ac5247cbabc47d8a8aefc6c00936
Tom Miksch <thomas-miksch@uiowa.edu>
2016-12-03 11:34:42 -0600

    Client stuff

--- src/ClientController.java
+++ src/ClientController.java
@@ -1,6 +1,31 @@
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.ChoiceBoxListCell;
+
+import javax.annotation.Resource;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by tmiksch on 12/3/16.
  */
 public class ClientController {
+    /*@FXML
+    private ChoiceBox stateChoiceBox;
+    @FXML
+    private ChoiceBox countyChoiceBox;
+    @FXML
+    private ChoiceBox partyChoiceBox;
+    @FXML
+    private TextField addressTextField;
+
+    @FXML
+    private void initialize() {
+        stateChoiceBox.setItems(FXCollections.observableArrayList("New Document", "Open "));
+    }*/
+
 
 }
