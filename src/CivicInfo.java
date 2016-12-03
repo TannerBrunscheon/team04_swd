6019e5d179ab26510343364169038b7b00bf4b88
Tanner <tanner-brunscheon@uiowa.edu>
2016-12-03 12:23:21 -0600

    Civics Added

---
+++ src/CivicInfo.java
@@ -0,0 +1,11 @@
+import com.google.api.services.civicinfo.model.RepresentativeInfoRequest;
+
+/**
+ * Created by Tanner on 12/3/2016.
+ */
+public class CivicInfo {
+    public static String  getRepInfo(String Address){
+        CivicInfo civicInfo = new CivicInfo.Bu
+        RepresentativeInfoRequest representativeInfoByAddress = new RepresentativeInfoRequest(Address)
+    }
+}
