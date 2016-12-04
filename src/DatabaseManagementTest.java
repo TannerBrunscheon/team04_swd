4f2f7aae5e2e1676112a525bca282c590921b8e3
Callum Duff <callum-duff@uiowa.edu>
2016-12-03 18:41:38 -0600

    added voting methods

--- src/DatabaseManagementTest.java
+++ src/DatabaseManagementTest.java
@@ -5,5 +5,10 @@ public class DatabaseManagementTest {
     public static void main(String[] args){
         DatabaseManagement.presidentialRaceVote("IA", "democrat");
         DatabaseManagement.presidentialRaceVote("IA", "democrat");
+        DatabaseManagement.presidentialRaceVote("IA", "democrat");
+        DatabaseManagement.presidentialRaceVote("IA", "democrat");
+
+        DatabaseManagement.presidentialRaceVote("IA", "republican");
+        DatabaseManagement.presidentialRaceVote("IA", "republican");
     }
 }
