ab0240d5c069bfb35a518a36a702bcd671cd0769
Tanner Brunscheon <tanner-brunscheon@uiowa.edu>
2016-12-04 17:11:46 -0600

    Last Fix

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -1,26 +1,30 @@
-/**
- * Created by tmiksch on 12/3/16.
- */
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Created by tbrunscheon on 12/3/16.
+ */
 public class ClientGUI extends Application {
+    /**
+     * Created by tmiksch on 12/3/16.
+     */
 
-    public static void main(String[] args) {
-        launch(args);
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+            Parent root =
+                    FXMLLoader.load(getClass().getResource("ClientFX.fxml"));
+            Scene scene = new Scene(root); // attach scene graph to scene
+            primaryStage.setTitle("Voter Address"); // displayed in window's title bar
+            primaryStage.setScene(scene); // attach scene to stage
+            primaryStage.show(); // display the stage
+        }
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root =
-                FXMLLoader.load(getClass().getResource("ClientFX.fxml"));
-        Scene scene = new Scene(root); // attach scene graph to scene
-        primaryStage.setTitle("Voter Address"); // displayed in window's title bar
-        primaryStage.setScene(scene); // attach scene to stage
-        primaryStage.show(); // display the stage
-    }
-}
