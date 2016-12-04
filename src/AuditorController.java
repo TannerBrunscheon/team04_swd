c5baed3ec8a8f0fa65df4ca207df64f33e878d68
Tom Miksch <thomas-miksch@uiowa.edu>
2016-12-03 18:30:16 -0600

    Client stuff

--- src/AuditorController.java
+++ src/AuditorController.java
@@ -1,6 +1,7 @@
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -12,8 +13,8 @@ import javafx.scene.control.TextField;
 public class AuditorController {
     @FXML
     private ChoiceBox raceDropDown;
-    //@FXML
-    //private TextField districtIDBox;
+    @FXML
+    private TextField districtIDBox;
     @FXML
     private TextField repubBox;
     @FXML
@@ -22,16 +23,22 @@ public class AuditorController {
     private TextField stateBox;
     @FXML
     private Button addButton;
+    @FXML
+    private Button pushPres;
+    @FXML
+    private Button pushSen;
+    @FXML
+    private Button pushHouse;
 
     @FXML
     private void initialize() {
-        String
         raceDropDown.setItems(FXCollections.observableArrayList("President","Senate","House"));
         raceDropDown.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
             @Override
             public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-                if (newValue != null){
-                    switch (newValue){
+                String raceToAudit = raceDropDown.getValue().toString();
+                if (raceToAudit != null){
+                    switch (raceToAudit){
                         case "President":
                             districtIDBox.setDisable(true);
                             stateBox.setDisable(true);
@@ -43,12 +50,30 @@ public class AuditorController {
                             break;
 
                         case "House":
+                            districtIDBox.setDisable(false);
+                            stateBox.setDisable(false);
+                            break;
 
                     }
 
                 }
             }
         });
+
+    }
+
+    @FXML
+    private void presPressed(ActionEvent event) throws Exception{
+
+    }
+
+    @FXML
+    private void senPressed(ActionEvent event) throws Exception{
+
+    }
+
+    @FXML
+    private void housePressed(ActionEvent event) throws Exception{
 
     }
 
