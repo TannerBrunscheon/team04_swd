95cada1d2a4e110f158148be6214e8376dac1c54
Callum Duff <callum-duff@uiowa.edu>
2016-12-04 16:22:53 -0600

    changed sql script

--- src/value_setter.sql
+++ src/value_setter.sql
@@ -1,3 +1,7 @@
+
+/*
+  This block below will randomly generate voting data for each race in each cell of the SQL database
+ */
 UPDATE presidentrace SET democrat = random()*(9000) + 1;
 UPDATE presidentrace SET republican = random()*(9000) + 1;
 
@@ -8,12 +12,14 @@ UPDATE senaterace SET democrat = random()*(9000) + 1;
 UPDATE senaterace SET republican = random()*(9000) + 1;
 
 
-
+/*
+  This block below will clear all the values from the database if executed
+ */
 UPDATE presidentrace SET democrat = 0;
 UPDATE presidentrace SET republican = 0;
 
-UPDATE houserace SET democrat = 0;
-UPDATE houserace SET republican = 0;
+UPDATE houserace SET democrat = 0, demcandidate = "";
+UPDATE houserace SET republican = 0, repcandidate = "";
 
-UPDATE senaterace SET democrat = 0;
-UPDATE senaterace SET republican = 0;
+UPDATE senaterace SET democrat = 0, demcandidate = "";
+UPDATE senaterace SET republican = 0, repcandidate = "";
